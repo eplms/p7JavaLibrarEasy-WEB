84145deaf7a966cf8861b8bc6c5d7ce20cba6a86
Emmanuel Plumas <eplumas@free.fr>
2020-07-08 18:43:48 +0200

    Création de l'appli web du P7 client de l'API

---
+++ src/main/java/com/emmanuel/plumas/p7JavaLibrarEasyWEB/P7JavaLibrarEasyWebApplication.java
@@ -0,0 +1,13 @@
+package com.emmanuel.plumas.p7JavaLibrarEasyWEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P7JavaLibrarEasyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P7JavaLibrarEasyWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emmanuel/plumas/p7JavaLibrarEasyWEB/P7JavaLibrarEasyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmanuel.plumas.p7JavaLibrarEasyWEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P7JavaLibrarEasyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
